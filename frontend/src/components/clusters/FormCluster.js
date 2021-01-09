34677a8a1043c80bf2de03027fee1a3d516e2acf
Zakurove <nasser.alharbi001@gmail.com>
2021-01-09 20:10:21 +0300

    Complete 'create cluster' page both front and backends

--- frontend/src/components/clusters/FormCluster.js
+++ frontend/src/components/clusters/FormCluster.js
@@ -1,47 +1,141 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { addCluster } from '../../actions/clusters.js';
+import { getSets } from '../../actions/sets.js';
 
 
  export class FormCluster extends Component {
+   
    state = {
      title: '',
      description: '',
      block: this.props.block,
      subject: this.props.subject,
+     sets: [],
+     setsArray: [],
+     isUpdating: true,
+     isPending: true,
+     isReady: false,
+     
    }
-     static propTypes = {
-       addCluster: PropTypes.func.isRequired,
-       block: PropTypes.string.isRequired,
-       subject: PropTypes.string.isRequired,
-       backToList: PropTypes.func.isRequired,
-     };
 
+   
+
+       //Before render, to fetch info about this list regarding subject and block
+  rendering() {
+    if (this.state.isUpdating == true) {
+      if (this.props.block == "Hematology/Oncology") {
+        this.setState({
+          blockLink: "hemOnc",
+        });
+      }
+      if (this.props.block !== "Hematology/Oncology") {
+        const blockLink = this.props.block.toLowerCase();
+        this.setState({
+          blockLink: blockLink,
+        });
+      }
+      const subjectLink = this.props.subject.toLowerCase();
+
+
+
+      this.setState({
+        subjectLink: subjectLink,
+        isUpdating: false,
+        isPending: true
+      });
+      this.props.getSets(this.state.block, this.state.subject);
+    }
+
+    //Pending 
+    if (this.state.isPending == true) {
+      // To give the 'sets' an isChecked field
+      let newSeti;
+      let newSets = [...this.props.sets]
+      for (newSeti = 0; newSeti < newSets.length; newSeti++) {
+        newSets[newSeti] = {...newSets[newSeti], isChecked: false}
+      }
+      this.setState ({
+        sets: newSets,
+        isPending: false
+      })
+    }
+    
+  }
+
+  static propTypes = {
+    sets: PropTypes.array.isRequired,
+    getSets: PropTypes.func.isRequired,
+    addCluster: PropTypes.func.isRequired,
+    block: PropTypes.string.isRequired,
+    subject: PropTypes.string.isRequired,
+    backToList: PropTypes.func.isRequired,
+  };
+
+  // this.handleCheckElement = this.handleCheckElement.bind(this);
+  handleCheckElement = (e) => {
+    let checkedSets = this.state.sets
+    checkedSets.forEach(set => {
+      if (set.id == e.target.value) {
+        set.isChecked =  e.target.checked
+      }
+      })
+    this.setState({sets: checkedSets})
+ }
      onChange = e => this.setState ({ [e.target.name]: e.target.value });
+
+
 
      onSubmit = (e) => {
        e.preventDefault();
+      
+      //Sets related to the cluster
+       this.state.sets.forEach(set => {
+        if (set.isChecked) {
+          let id = set.id
+          this.setState(previousState => ({
+            setsArray: [...previousState.setsArray, id ]
+        }));
+        }
+       })
+
        const cluster = new FormData();
        cluster.append('title', this.state.title)
        cluster.append('description', this.state.description);
-       cluster.append('setsArray', ['2','3']);
+       setTimeout(() => cluster.append('setsArray', this.state.setsArray), 200);
        cluster.append('block', this.props.block);
        cluster.append('subject', this.props.subject);
-       this.props.addCluster(cluster);
+      //  this.props.addCluster(cluster);
+       setTimeout(() => this.props.addCluster(cluster), 400);
        this.setState({
          title: "",
          description: "",
        })
        
       //  Go to associate sets
-       this.props.backToList()
+      setTimeout(() => this.props.backToList(), 600);
+       
 
      };
+     componentDidMount() {
+      this.props.getSets(this.props.block, this.props.subject);
+
+    }
      render() {
+      
+      // The loading handler: isReady(For loading screen) > isUpdating(for fetching sets) > isPending (for adding 'isChecked' for sets) > Then finally loading the page
+    if (this.state.isReady == false) {
+      setTimeout(() => this.setState({ isReady: true }), 600);
+      }
+    if (this.state.isReady == true) {
+      this.rendering(); 
+    }
        const {title, description } = this.state;
-       return (
+       if (this.state.isPending == false) {
+        this.rendering(); 
+        return (
          <div className="container">
            <h1 className="text-center py-2 text-info">{this.props.block} {this.props.subject}: create a cluster</h1>
            <Button
@@ -80,18 +174,85 @@ import { addCluster } from '../../actions/clusters.js';
                 />
               </div>
  
-              <div className="form-group">
-              <button type="submit" className="btn btn-lg btn-warning btn-block">
-                Next
-              </button>
-            </div>
+
  
             </form>
             </div>
+            <div className="col-6" >
+            <h4 className="text-info">Select sets for this cluster:</h4>
+            <div style={{ height: "350px", overflow: "auto" }}>
+            <table className="table table-striped " > 
+            <thead>
+              <tr>
+                <th></th>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Owner</th>
+                 </tr>
+            </thead>
+            <tbody>
+              {this.state.sets.map((set) => (
+                <tr key={set.id}>
+                  <td><input key={set.id} onChange={ (e) => this.handleCheckElement(e) }  type="checkbox" checked={set.isChecked} value={set.id}   /></td>
+                  <td>{set.id}</td>
+                  <td>{set.title}</td>
+                  <td>{set.owner_username}</td>
+                  {/* {console.log(set)} */}
+
+                  {/* <td>
+                    <a
+                      href={`#/${this.state.blockLink}/${this.state.subjectLink}/${cluster.id}`}
+                      className="btn btn-warning"
+                      style={{ whiteSpace: "nowrap" }}
+                      onClick={(e) => {
+                        this.setState({
+                          selectedClusterId: cluster.id,
+                          // isViewing: true,
+                          selectedCluster: cluster,
+                        });
+                      }}
+                    >
+                      View Cluster
+                    </a>
+                  </td> */}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+             </div>
+            </div>
           </div>
+          <div className="row pt-4">     
+              {/* <div className="form-group"> */}
+              <button type="submit" form="clusterForm" className="btn btn-outline-info btn-lg btn-block">
+                Create this cluster
+              </button>
+            {/* </div> */}
+            </div>
           </div>
        )
+        }
+        if (this.state.isPending == true) {
+          return (
+      
+          <Fragment>
+      <div className="cssload-loader mt-5">
+        <div className="cssload-inner cssload-one"></div>
+        <div className="cssload-inner cssload-two"></div>
+        <div className="cssload-inner cssload-three"></div>
+      </div>
+          </Fragment>
+      
+          );
+          }
      }
    }
 
-  export default connect(null, { addCluster })(FormCluster);
+   const mapStateToProps = (state) => ({
+    // the first one is whatever we're getting so it's okay, the 2nd one is the name of the reducer, the 3rd the state in the reducer
+    sets: state.sets.sets,
+    // auth: state.auth,
+    // loadingState: state.loadingState,
+  });
+  // export default connect(null, { addCluster })(FormCluster);
+  export default connect(mapStateToProps, { getSets, addCluster })(FormCluster);
